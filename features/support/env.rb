6758ab0de743bfb1f79a59655ff0cd94a85c11d7
Frank G. Ficnar <frank.ficnar@mosaic.com>
2011-11-30 15:14:24 -0500

    Improved dump rake tasks, enhanced logon feature test (still broken)

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,7 +3,7 @@ require 'capybara/cucumber'
 require 'capybara/dsl'
 
 Capybara.register_driver :chrome do |app|
-  Capybara::Selenium::Driver.new(app, :browser => :firefox)
+  Capybara::Selenium::Driver.new(app, :browser => :chrome)
 end
 
 Capybara.default_driver = :chrome
