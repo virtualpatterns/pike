212830bb01087d793409fb34168d04dbf8c3b606
Frank G. Ficnar <frank.ficnar@gmail.com>
2012-01-14 18:25:02 -0500

    Modfying schedule

--- schedule.rb
+++ schedule.rb
@@ -1,4 +1,4 @@
-job_type :rake, 'cd :path && rake :task :output'
+job_type :rake, 'cd :path && bundle exec rake :task :output'
 
 every 1.minutes do
   rake "pike:data:actions:process_all", :output => { :standard => 'log/actions.log', :error => 'log/actions.error.log' }
