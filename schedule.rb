4ad67a0ec17fe70f184a0d879679b84bd94395b6
Frank G. Ficnar <frank.ficnar@gmail.com>
2012-01-15 16:28:00 -0500

    Updating rakefile and schedule.rb

--- schedule.rb
+++ schedule.rb
@@ -1,5 +1,5 @@
 job_type :rake, 'cd :path && bundle exec rake :task :output'
 
 every 1.minutes do
-  rake "pike:data:actions:process_all", :output => { :standard => 'log/actions.log', :error => 'log/actions.error.log' }
+  rake "pike:data:actions:process_all", :output => { :standard => 'log/pike_data_actions_process_all.log', :error => 'log/pike_data_actions_process_all.error.log' }
 end
