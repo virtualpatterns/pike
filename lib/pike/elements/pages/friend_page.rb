f200cfc30dea4c3f78ed012859f968a6592c510c
Frank G. Ficnar <frank.ficnar@gmail.com>
2012-01-20 00:18:07 -0500

    Deleting element cache

--- lib/pike/elements/pages/friend_page.rb
+++ lib/pike/elements/pages/friend_page.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'bundler/setup'
 
 require 'ruby_app/elements/button'
+require 'ruby_app/elements/dialogs/acknowledgement_dialog'
 require 'ruby_app/elements/dialogs/confirmation_dialog'
 require 'ruby_app/elements/dialogs/exception_dialog'
 require 'ruby_app/elements/navigation/back_button'
@@ -29,15 +30,19 @@ module Pike
           @delete_button.clicked do |element, event|
             Pike::Session.show_dialog(event, RubyApp::Elements::Dialogs::ConfirmationDialog.new('Confirm', 'Are you sure you want to remove this friend?')) do |_event, response|
               if response
-                RubyApp::Elements::Dialogs::ExceptionDialog.show_dialog(_event) do
-                  Pike::Friendship.where_friendship(@friendship.user_source, @friendship.user_target).each do |_friendship|
-                    _friendship.destroy
+                Pike::Session.show_dialog(_event, RubyApp::Elements::Dialogs::AcknowledgementDialog.new('Friend', 'This friend will be removed.  Any shared data will be removed momentarily.')) do |__event, response|
+                  if response
+                    RubyApp::Elements::Dialogs::ExceptionDialog.show_dialog(__event) do
+                      Pike::Friendship.where_friendship(@friendship.user_source, @friendship.user_target).each do |_friendship|
+                        _friendship.destroy
+                      end
+                      Pike::Friendship.where_friendship(@friendship.user_target, @friendship.user_source).each do |_friendship|
+                        _friendship.destroy
+                      end
+                      Pike::Session.pages.pop
+                      __event.refresh
+                    end
                   end
-                  Pike::Friendship.where_friendship(@friendship.user_target, @friendship.user_source).each do |_friendship|
-                    _friendship.destroy
-                  end
-                  Pike::Session.pages.pop
-                  _event.refresh
                 end
               end
             end
