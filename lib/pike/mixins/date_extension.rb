2fe306ecb3493b51e6e6ef8979020da81dabae07
Frank G. Ficnar <frank.ficnar@mosaic.com>
2012-06-26 13:26:12 -0400

    Fixing report issue on Sunday

--- lib/pike/mixins/date_extension.rb
+++ lib/pike/mixins/date_extension.rb
@@ -8,9 +8,9 @@ module Pike
         return self - self.wday
       end
 
-      #def end_of_week
-      #  return self + (6 - self.wday)
-      #end
+      def end_of_week
+        return self + (6 - self.wday)
+      end
 
     end
 
