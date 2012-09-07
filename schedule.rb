8fba82b176b5146f81858be5bb0ec3e8ca0d78ba
Frank G. Ficnar <frank.ficnar@gmail.com>
2012-09-07 13:33:35 -0400

    Mucking around with whenever

--- schedule.rb
+++ schedule.rb
@@ -1,5 +1,5 @@
-job_type :rake, 'cd :path; mkdir -p ./process/cron/log; PATH=:PATH RUBY_APP_CONFIGURATION=:RUBY_APP_CONFIGURATION bundle exec rake :task --silent :output'
+job_type :rake, 'cd :path; mkdir -p ./process/cron/log; PATH=:PATH bundle exec rake :task --silent :output'
 
-every 1.hour do
+every 1.minute do
   rake 'pike:data:backup', :output => './process/cron/log/backup.log'
 end
