7cd414cb015ca8d0e9f9d9141006f1bb5667aa16
Frank G. Ficnar <frank.ficnar@gmail.com>
2012-11-03 00:58:25 -0400

    Updating schedule

--- tasks/schedule.rb
+++ tasks/schedule.rb
@@ -1,9 +1,9 @@
 job_type :rake, 'cd :path; mkdir -p ./process/cron/log; PATH=:PATH bundle exec rake :task --silent :output'
 
-every 1.day do
+every :day, :at => '12:30AM' do
   rake 'pike:data:log:rotate',  :output => './process/cron/log/rotate.log'
 end
 
-every 1.hour do
+every :hour do
   rake 'pike:data:backup',      :output => './process/cron/log/backup.log'
 end
