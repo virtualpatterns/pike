9c67f06d6b8b74535664df24f0d8b7830d61b68a
virtualpatterns <frank.ficnar@gmail.com>
2013-01-09 16:05:31 -0500

    Updating gems and logon scripts

--- scripts/logon.rb
+++ scripts/logon.rb
@@ -1,19 +1,20 @@
 add_step! (RubyApp::Element::Event)                           { |event| event.execute {} }
-add_step! (RubyApp::Element::ExecutedEvent)                   { |event| event.assert_exists_link('Logon with GitHub') }
-add_step! (RubyApp::Element::AssertedEvent)                   { |event| event.click_link('Logon with GitHub') }
-add_step! (RubyApp::Elements::Mobile::Page::ShownEvent)       { |event| event.assert_exists_link('tap to add a task') }
-add_step! (RubyApp::Element::AssertedEvent)                   { |event| event.assert_exists_link('Logoff') }
-add_step! (RubyApp::Element::AssertedEvent)                   { |event| event.click_link('Logoff') }
-add_step! (RubyApp::Elements::Mobile::Page::ShownEvent)       { |event| event.assert_exists_link('Logon with GitHub') }
-add_step! (RubyApp::Element::AssertedEvent)                   { |event| event.execute {} }
 
-add_step! (RubyApp::Element::ExecutedEvent)                   { |event| event.assert_exists_link('Logon with Google') }
-add_step! (RubyApp::Element::AssertedEvent)                   { |event| event.click_link('Logon with Google') }
-add_step! (RubyApp::Elements::Mobile::Page::ShownEvent)       { |event| event.assert_exists_link('tap to add a task') }
-add_step! (RubyApp::Element::AssertedEvent)                   { |event| event.assert_exists_link('Logoff') }
-add_step! (RubyApp::Element::AssertedEvent)                   { |event| event.click_link('Logoff') }
-add_step! (RubyApp::Elements::Mobile::Page::ShownEvent)       { |event| event.assert_exists_link('Logon with Google') }
-add_step! (RubyApp::Element::AssertedEvent)                   { |event| event.execute {} }
+# add_step! (RubyApp::Element::ExecutedEvent)                   { |event| event.assert_exists_link('Logon with GitHub') }
+# add_step! (RubyApp::Element::AssertedEvent)                   { |event| event.click_link('Logon with GitHub') }
+# add_step! (RubyApp::Elements::Mobile::Page::ShownEvent)       { |event| event.assert_exists_link('tap to add a task') }
+# add_step! (RubyApp::Element::AssertedEvent)                   { |event| event.assert_exists_link('Logoff') }
+# add_step! (RubyApp::Element::AssertedEvent)                   { |event| event.click_link('Logoff') }
+# add_step! (RubyApp::Elements::Mobile::Page::ShownEvent)       { |event| event.assert_exists_link('Logon with GitHub') }
+# add_step! (RubyApp::Element::AssertedEvent)                   { |event| event.execute {} }
+
+# add_step! (RubyApp::Element::ExecutedEvent)                   { |event| event.assert_exists_link('Logon with Google') }
+# add_step! (RubyApp::Element::AssertedEvent)                   { |event| event.click_link('Logon with Google') }
+# add_step! (RubyApp::Elements::Mobile::Page::ShownEvent)       { |event| event.assert_exists_link('tap to add a task') }
+# add_step! (RubyApp::Element::AssertedEvent)                   { |event| event.assert_exists_link('Logoff') }
+# add_step! (RubyApp::Element::AssertedEvent)                   { |event| event.click_link('Logoff') }
+# add_step! (RubyApp::Elements::Mobile::Page::ShownEvent)       { |event| event.assert_exists_link('Logon with Google') }
+# add_step! (RubyApp::Element::AssertedEvent)                   { |event| event.execute {} }
 
 add_step! (RubyApp::Element::ExecutedEvent)                   { |event| event.go('/') }
 add_step! (RubyApp::Elements::Mobile::Page::LoadedEvent)      { |event| event.assert_exists_link('Logon with Facebook') }
