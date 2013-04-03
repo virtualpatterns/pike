42a5ccf25b5c4823324b146d8e79fcdbfbdd125f
virtualpatterns <code@virtualpatterns.com>
2013-04-03 01:21:48 -0400

    Updating cache, removing Scripts link from default script

--- scripts/default.rb
+++ scripts/default.rb
@@ -1,13 +1,5 @@
 add_step! (RubyApp::Element::Event)                           { |event| event.execute {} }
 
-add_step! (RubyApp::Element::ExecutedEvent)                   { |event| event.assert_exists_link('Scripts') }
-add_step! (RubyApp::Element::AssertedEvent)                   { |event| event.click_link('Scripts') }
-add_step! (RubyApp::Elements::Mobile::Page::ShownEvent)       { |event| event.assert_exists_text('Scripts') }
-add_step! (RubyApp::Element::AssertedEvent)                   { |event| event.assert_exists_link('Back') }
-add_step! (RubyApp::Element::AssertedEvent)                   { |event| event.click_link('Back') }
-add_step! (RubyApp::Elements::Mobile::Page::ShownEvent)       { |event| event.assert_exists_link('Scripts') }
-add_step! (RubyApp::Element::AssertedEvent)                   { |event| event.execute {} }
-
 add_step! (RubyApp::Element::ExecutedEvent)                   { |event| event.assert_exists_link('First') }
 add_step! (RubyApp::Element::AssertedEvent)                   { |event| event.click_link('First') }
 add_step! (RubyApp::Elements::Mobile::Page::ShownEvent)       { |event| event.assert_exists_link('tap to add a task') }
