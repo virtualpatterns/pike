8c982e48e5dec469c69f1ec60f335aa2473ccc2a
virtualpatterns <code@virtualpatterns.com>
2013-04-03 23:39:32 -0400

    Updating script tasks

--- tasks/script.rb
+++ tasks/script.rb
@@ -2,91 +2,101 @@ namespace :pike do
 
   namespace :script do
 
-    desc 'Run a given script using a given number of instances of PhantomJS on a given url'
-    task :run, :url, :script, :_count do |task, arguments|
+    desc 'Start a given script using a given number of instances of PhantomJS on a given url'
+    task :start, :url, :script, :_count do |task, arguments|
+      Rake::Task['pike:script:log:destroy'].invoke(arguments.script)
       _count = arguments._count ? arguments._count.to_i : 1
-      puts "Starting #{_count} instance(s), sending output to scripts/#{arguments.script}.log ..." if _count > 1
+      puts "Starting #{_count} instance(s), sending output to scripts/#{arguments.script}.log ..."
       (1.._count).each do |count|
-        system("phantomjs --ignore-ssl-errors=true scripts/phantom/phantom.js #{arguments.url} #{arguments.script} #{_count <= 1 ? '' : "1>> scripts/#{arguments.script}.log 2>> /dev/null &"}")
+        system("phantomjs --ignore-ssl-errors=true scripts/phantom/phantom.js #{arguments.url} #{arguments.script} 1>> scripts/#{arguments.script}.log 2>> /dev/null &")
       end
+    end
+
+    namespace :log do
+
+      desc 'Destroy the log for a given script'
+      task :destroy, :script do |task, arguments|
+        system("rm scripts/#{arguments.script}.log")
+      end
+
+      desc 'Monitor the log for a given script'
+      task :monitor, :script do |task, arguments|
+        puts "Monitoring #{arguments.script}, CTRL+C to stop"
+        system("tail -f scripts/#{arguments.script}.log")
+      end
+
     end
 
     namespace :standard_no_logon do
 
-      desc 'Run the script through PhantomJS on a given url'
-      task :run, :url do |task, arguments|
-        Rake::Task['pike:script:run'].invoke(arguments.url, 'phantom/standard_no_logon')
+      desc 'Start the script through PhantomJS on a given url'
+      task :start, :url do |task, arguments|
+        Rake::Task['pike:script:start'].invoke(arguments.url, 'phantom/standard_no_logon')
       end
 
-      desc 'Run the script on the local environment'
+      desc 'Start the script on the local environment'
       task :local do |task|
-        Rake::Task['pike:script:standard_no_logon:run'].invoke('http://localhost:8000/pike')
+        Rake::Task['pike:script:standard_no_logon:start'].invoke('http://localhost:8000/pike')
       end
 
-      desc 'Run the script on the development environment'
+      desc 'Start the script on the development environment'
       task :development do |task|
-        Rake::Task['pike:script:standard_no_logon:run'].invoke('https://development.virtualpatterns.com/pike')
+        Rake::Task['pike:script:standard_no_logon:start'].invoke('https://development.virtualpatterns.com/pike')
       end
 
-      desc 'Run the script on the staging environment'
+      desc 'Start the script on the staging environment'
       task :staging do |task|
-        Rake::Task['pike:script:standard_no_logon:run'].invoke('https://code.virtualpatterns.com/pike')
+        Rake::Task['pike:script:standard_no_logon:start'].invoke('https://code.virtualpatterns.com/pike')
       end
 
     end
 
     namespace :performance do
 
-      desc 'Run the script using a given number of instances of PhantomJS on a given url'
-      task :run, :url, :_count do |task, arguments|
-        _count = arguments._count ? arguments._count.to_i : 1
-        if _count > 1
-          system("rm scripts/phantom/performance.log")
-        end
-        Rake::Task['pike:script:run'].invoke(arguments.url, 'phantom/performance', _count)
-        if _count > 1
-          system("tail -f scripts/phantom/performance.log")
-          system("cat scripts/phantom/performance.log | scripts/phantom/performance.awk")
-        end
+      desc 'Start the script using a given number of instances of PhantomJS on a given url'
+      task :start, :url, :_count do |task, arguments|
+        Rake::Task['pike:script:start'].invoke(arguments.url, 'phantom/performance', arguments._count)
+        Rake::Task['pike:script:log:monitor'].invoke('phantom/performance')
       end
 
-      desc 'Run the script on the local environment'
+      desc 'Start the script on the local environment'
       task :local, :_count do |task, arguments|
-        Rake::Task['pike:script:performance:run'].invoke('http://localhost:8000/pike', arguments._count)
+        Rake::Task['pike:script:performance:start'].invoke('http://localhost:8000/pike', arguments._count)
       end
 
-      desc 'Run the script on the development environment'
+      desc 'Start the script on the development environment'
       task :development, :_count do |task, arguments|
-        Rake::Task['pike:script:performance:run'].invoke('https://development.virtualpatterns.com/pike', arguments._count)
+        Rake::Task['pike:script:performance:start'].invoke('https://development.virtualpatterns.com/pike', arguments._count)
       end
 
-      desc 'Run the script on the staging environment'
+      desc 'Start the script on the staging environment'
       task :staging, :_count do |task, arguments|
-        Rake::Task['pike:script:performance:run'].invoke('https://code.virtualpatterns.com/pike', arguments._count)
+        Rake::Task['pike:script:performance:start'].invoke('https://code.virtualpatterns.com/pike', arguments._count)
       end
 
     end
 
     namespace :performance_continuous do
 
-      desc 'Run the script using a given number of instances of PhantomJS on a given url'
-      task :run, :url, :_count do |task, arguments|
-        Rake::Task['pike:script:run'].invoke(arguments.url, 'phantom/performance_continuous', arguments._count)
+      desc 'Start the script using a given number of instances of PhantomJS on a given url'
+      task :start, :url, :_count do |task, arguments|
+        Rake::Task['pike:script:start'].invoke(arguments.url, 'phantom/performance_continuous', arguments._count)
+        Rake::Task['pike:script:log:monitor'].invoke('phantom/performance_continuous')
       end
 
-      desc 'Run the script on the local environment'
+      desc 'Start the script on the local environment'
       task :local, :_count do |task, arguments|
-        Rake::Task['pike:script:performance_continuous:run'].invoke('http://localhost:8000/pike', arguments._count)
+        Rake::Task['pike:script:performance_continuous:start'].invoke('http://localhost:8000/pike', arguments._count)
       end
 
-      desc 'Run the script on the development environment'
+      desc 'Start the script on the development environment'
       task :development, :_count do |task, arguments|
-        Rake::Task['pike:script:performance_continuous:run'].invoke('https://development.virtualpatterns.com/pike', arguments._count)
+        Rake::Task['pike:script:performance_continuous:start'].invoke('https://development.virtualpatterns.com/pike', arguments._count)
       end
 
-      desc 'Run the script on the staging environment'
+      desc 'Start the script on the staging environment'
       task :staging, :_count do |task, arguments|
-        Rake::Task['pike:script:performance_continuous:run'].invoke('https://code.virtualpatterns.com/pike', arguments._count)
+        Rake::Task['pike:script:performance_continuous:start'].invoke('https://code.virtualpatterns.com/pike', arguments._count)
       end
 
     end
